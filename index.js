80c6414fd32ea9abef911dd727adc6cb403dbe4d
psejun <devpark001@gmail.com>
2023-05-17 21:02:51 +0900

    env

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require("dotenv").config();
 const app = express()
 const port = 5000
-mongoose.connect('mongodb+srv://bakssse:Qkrwns1313!@boilerplate.cm1ktua.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://bakssse:'+process.env.MONGOOSE_PASSWORD+'@boilerplate.cm1ktua.mongodb.net/?retryWrites=true&w=majority', {
     // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(()=> console.log('MongoDB Connected...'))
+}).then(()=> console.log('MongoDB Connected Ok'))
   .catch(err => console.log(err))
 
 // 1
