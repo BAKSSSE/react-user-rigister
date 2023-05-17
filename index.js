5de64237a6d198cbb677df8b45a4d045b7cda7bd
psejun <devpark001@gmail.com>
2023-05-17 14:50:11 +0900

    init1

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 5000
+mongoose.connect('mongodb+srv://bakssse:Qkrwns1313!@boilerplate.cm1ktua.mongodb.net/?retryWrites=true&w=majority', {
+    // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(()=> console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
